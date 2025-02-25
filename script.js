0838a28b2e38f5bfbaedf394feeadb2812d1c105
pushkar2510 <pushkargaikwad25@gmail.com>
2025-02-25 22:07:44 +0530

    updated about us section

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', function () {
     const header = document.querySelector('.header');
     if (window.scrollY > 50) { // Adjust the 50px value based on when you want the blur to happen
-        header.classList.add('blur');
+        header.classList.add('navblur');
     } else {
-        header.classList.remove('blur');
+        header.classList.remove('navblur');
     }
 });
